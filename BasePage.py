88a047e132d9f67cd93a4424d7d5a7aaca1491fc
Ken-Kei <liujianjianm@hotmail.com>
2016-09-02 21:41:16 +0800

    优化代码

--- BasePage.py
+++ BasePage.py
@@ -7,14 +7,14 @@ Create Date  :  2016/07/01
 
 
 from selenium import webdriver
-from ThirdParty.selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support.wait import WebDriverWait
 from attribute import *  # @UnusedWildImport
 from common import CommonUtils
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import logging
 from selenium.common.exceptions import NoSuchElementException
 from LogInfo import *
-from ThirdParty.selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support import expected_conditions as ec
 
 com = CommonUtils()
 
