209cb248e00e85929aad3b36e97016b1b375e35c
Ken-Kei <liujianjianm@hotmail.com>
2016-09-21 16:48:20 +0800

    automate store page

--- OperationPage.py
+++ OperationPage.py
@@ -2,7 +2,7 @@
 
 """
 Author       :  Ken-Kei
-Create Date  :  2016/7/1
+Create Date  :  2016/07/01
 """
 
 from selenium.common.exceptions import NoSuchElementException
---
+++ StorePage.py
@@ -0,0 +1,16 @@
+# -*- coding: UTF-8 -*-
+
+"""
+Author       :  Ken-Kei
+Create Date  :  2016/09/21
+"""
+
+from selenium.common.exceptions import NoSuchElementException
+from attribute import *  # @UnusedWildImport
+from OperationLocators import *
+from LogInfo import *
+from BasePage import BasePage
+import logging
+import datetime
+import time
+from selenium.webdriver.common.keys import Keys
